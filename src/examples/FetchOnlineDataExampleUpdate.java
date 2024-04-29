fd56d934025ff4aae46a66f998d8c40121673a72
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-04-28 23:09:28 -0600

    finalizes method reconstruction inits with javadoc inits
    opted to use the Qid search to be singular with hints of getting plural resutls
    
    opted to use the byTitle section to example getting with multiple titles to search and hints of getting singular results

--- src/examples/FetchOnlineDataExampleUpdate.java
+++ src/examples/FetchOnlineDataExampleUpdate.java
@@ -38,11 +38,10 @@ import org.wikidata.wdtk.wikibaseapi.apierrors.MediaWikiApiErrorException;
  * This example demonstrates fetching data from the Wikidata.org API and covers fetching entity data, and applying various filters to reduce the volume of data returned. 
  * 
  *<ul>
-  * <li> Fetching data for a single entity, or multiple entities, using entity Qids (e.g. "Q42" and "P31"). </li>
-  * <li> Fetching data for entities applying filters: This example shows how to apply filters to reduce the volume of fetched data. It sets filters for: site links, language, and propertities, then fetches data for a single entity with Qid "Q8", then prints its French label and English Wikipedia page title. </li>
+  * <li> Fetching data for a single entity and multiple entities, using entity Qids (e.g. "Q42" and "P31"). </li>
   * <li>Fetching by Page Title: Fetches data for a single entity with the page titles (e.g. "Terry Pratchett" on English Wikipedia), then prints the correspinding Qid. </li>
-  *<li>Fetching by Multiple Page Titles: Fetches data for multiple entities with the page titles (e.g. "Wikidata" and "Wikipedia" on English Wikipedia), then prints the correspinding Qids. </li>
-  *<li> Searching for Entities: Searches for entities with a specific label (e.g. "Douglas Adams") and prints the Qids and labels of the search results. </li>
+  *<li> Searching for Entities: Searches for entities using a search term (e.g. "Douglas Adams") and which language Wiki to search (e.g. "fr"),  then prints the Qids and labels of the search results. </li>
+  * <li> Fetching data for entities applying filters: This example shows how to apply filters to reduce the volume of fetched data. It sets filters for: site links, language, and propertities, then fetches data for a single entity with Qid "Q8", then prints its French label and English Wikipedia page title. </li>
   *</ul>
   *
   * @throws MediaWikiApiErrorException
@@ -61,8 +60,10 @@ public class FetchOnlineDataExampleUpdate {
         BasicApiConnection.getWikidataApiConnection(),
         Datamodel.SITE_WIKIDATA);
 
-    fetchEntityDataByQid(wbdf);
-    fetchEntitiesDataByQid(wbdf);
+    fetchEntityDataByQid(wbdf); // get entities using their Qids...
+    fetchEntitiesByTitles(wbdf); // get entities using their page titles...
+    fetchEntityBySearchTerm(wbdf); // search for entities using query text...
+    fetchEntityAndApplyFilters(wbdf); // get entities applying filters...
   }
 
   /**
@@ -72,54 +73,93 @@ public class FetchOnlineDataExampleUpdate {
   * @param wbdf An instance of WikibaseDataFetcher that is used to fetch the data.
   * @throws MediaWikiApiErrorException If there is an error while fetching the data from the API.
   * @throws IOException If there is an error while writing the fetched data to a file.
+  
+    @see #WikibaseDataFetcher.getEntityDocuments(String[] qids)
   */
   public static void fetchEntityDataByQid(WikibaseDataFetcher wbdf) throws MediaWikiApiErrorException, IOException {
     System.out.println("*** Fetching data for one entity:");
+
+    //? Multiple entities can be fetched using the plural getEntityDocuments() method.
     EntityDocument q42 = wbdf.getEntityDocument("Q42");
-    writeEntityDataToFile(q42, "entity-Q42.txt");
+
     if (q42 instanceof ItemDocument) {
+      writeEntityDataToFile(q42, "entity-Q42.txt");
       System.out.println("The English name for entity Q42 is "
           + ((ItemDocument) q42).getLabels().get("en").getText());
+      System.out.println("Raw data for entity Q42 written to file entity-Q42.txt");
+    } else {
+      System.out.println("Entity Q42 was not found!");
     }
-    System.out.println("Raw data for entity Q42 written to file entity-Q42.txt");
   }
 
   /**
-  * This method fetches data for multiple entities from the Wikidata.org API using their QID's (unique identifier).
-  * The fetched data is then written to a file and the Qid's are printed to the console.
+   * This method fetches data for a single entity from the Wikidata.org API using an entities title.
+   * The fetched data is then written to a file and the Qid's and labels of the search results are printed to the console.
+   * 
+   * @param wbdf An instance of WikibaseDataFetcher that is used to fetch the data.
+   * @throws MediaWikiApiErrorException If there is an error while fetching the data from the API.
+   * @throws IOException If there is an error while writing the fetched data to a file.
+   * 
+   * @see #WikibaseDataFetcher.getEntityDocumentByTitle(String siteIri, String pageTitle)
+   */
+  public static void fetchEntitiesByTitles(WikibaseDataFetcher wbdf) throws MediaWikiApiErrorException, IOException {
+    System.out.println("*** Fetching data based on page title:");
+
+    //? Singular entities, similar to above, can be fetched using the singular getEntityDocumentByTitle() method.
+    Map<String, EntityDocument> results = wbdf.getEntityDocumentsByTitle("enwiki",
+        "Terry Pratchett", "Neil Gaiman");
+    for (Entry<String, EntityDocument> entry : results.entrySet()) {
+      writeEntityDataToFile(entry.getValue(), "entity-" + entry.getKey() + ".txt");
+      System.out.println("The Qid of " + entry.getKey() + " is " + entry.getValue().getEntityId().getId());
+      System.out
+          .println("Raw data for entity " + entry.getKey() + " written to file entity-" + entry.getKey() + ".txt");
+    }
+  }
+
+  /**
+  * This method fetches data for a single entity from the Wikidata.org API using a search term and language target or iri.
+  * The fetched data's Qid and labels are then printed to the console.
+  * 
+  * @param wbdf An instance of WikibaseDataFetcher that is used to fetch the data.
+  * @throws MediaWikiApiErrorException If there is an error while fetching the data from the API.
+  * @throws IOException If there is an error while writing the fetched data to a file.
+  */
+  public static void fetchEntityBySearchTerm(WikibaseDataFetcher wbdf) throws MediaWikiApiErrorException, IOException {
+    System.out.println("*** Searching for entities:");
+    for (WbSearchEntitiesResult result : wbdf.searchEntities("Douglas Adams", "fr")) {
+      System.out.println("Found entity " + result.getTitle() + " with Qid "
+          + result.getEntityId());
+    }
+  }
+
+  /**
+  * This method fetches data for a single entity, then applies filters by selecting only site links from English Wikipedia, labels in French, which have no statements at all.
+  * The fetched data is then written to a file and the French label and English Wikipedia page title are printed to the console.
   *
   * @param wbdf An instance of WikibaseDataFetcher that is used to fetch the data.
   * @throws MediaWikiApiErrorException If there is an error while fetching the data from the API.
   * @throws IOException If there is an error while writing the fetched data to a file.
   */
-  public static void fetchEntitiesDataByQid(WikibaseDataFetcher wbdf) throws MediaWikiApiErrorException, IOException {
-    System.out.println("*** Fetching data for several entities:");
-    Map<String, EntityDocument> results = wbdf.getEntityDocuments("Q80",
-        "P31");
-    // Keys of this map are Qids, but we only use the values here:
-    for (EntityDocument ed : results.values()) {
-      System.out.println("Successfully retrieved data for "
-          + ed.getEntityId().getId());
-    }
-    writeEntityDataToFile(results.values().toArray(new EntityDocument[0]), "entities-Q80-P31.txt");
-    System.out.println("Raw data for entities Q80 and P31 written to file entities-Q80-P31.txt");
-  }
+  public static void fetchEntityAndApplyFilters(WikibaseDataFetcher wbdf)
+      throws MediaWikiApiErrorException, IOException {
+    System.out.println("*** Fetching data for entities applying filters:");
 
-  /**
-  * This method writes an array of EntityDocument objects to a file. It uses a PrintStream to write each 
-  * EntityDocument to the file specified by fileName. If an IOException occurs during this process, it prints 
-  * the stack trace.
-  *
-  * @param entityDocuments An array of EntityDocument objects to be written to the file.
-  * @param fileName The name of the file to which the entity data will be written.
-  */
-  private static void writeEntityDataToFile(EntityDocument[] entityDocuments, String fileName) {
-    try (PrintStream out = new PrintStream(ExampleHelpers.openExampleFileOuputStream(fileName))) {
-      for (EntityDocument entityDocument : entityDocuments) {
-        out.println(entityDocument);
-      }
-    } catch (IOException e) {
-      e.printStackTrace();
+    //? Filters can be applied to reduce the volume of fetched data.
+    //? Filters can be set for site links, language, and properties.
+    wbdf.getFilter().setSiteLinkFilter(Collections.singleton("enwiki")); // Only site links from English Wikipedia
+    wbdf.getFilter().setLanguageFilter(Collections.singleton("fr")); // Only labels in French
+    wbdf.getFilter().setPropertyFilter(Collections.emptySet()); // No statements at all
+    EntityDocument q8 = wbdf.getEntityDocument("Q8");
+
+    if (q8 instanceof ItemDocument) {
+      writeEntityDataToFile(q8, "entity-Q8.txt");
+      System.out.println("The French label for entity Q8 is "
+          + ((ItemDocument) q8).getLabels().get("fr").getText()
+          + "\nand its English Wikipedia page has the title "
+          + ((ItemDocument) q8).getSiteLinks().get("enwiki").getPageTitle() + ".");
+      System.out.println("Raw data for entity Q8 written to file entity-Q8.txt");
+    } else {
+      System.out.println("Entity Q8 was not found!");
     }
   }
 
