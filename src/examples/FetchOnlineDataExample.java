1ec9d64807d3859ba008da4326c721dd1ada0442
Horacio Velvetine <78946408+horaciovelvetine@users.noreply.github.com>
2024-05-06 18:24:46 -0600

    adds missing javadoc comments, reformats for beginner friendly readability

--- src/examples/FetchOnlineDataExample.java
+++ src/examples/FetchOnlineDataExample.java
@@ -22,6 +22,7 @@ package examples;
 
 import java.io.IOException;
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
@@ -33,89 +34,124 @@ import org.wikidata.wdtk.wikibaseapi.WbSearchEntitiesResult;
 import org.wikidata.wdtk.wikibaseapi.WikibaseDataFetcher;
 import org.wikidata.wdtk.wikibaseapi.apierrors.MediaWikiApiErrorException;
 
-public class FetchOnlineDataExample {
+/**
+  * This example demonstrates fetching data from the Wikidata.org API by their QID, title, or query and applying various filters to reduce the volume of data returned. 
+  * 
+  * This example does not download any dump files.
+ *<ul>
+  * <li> Fetches data for a single entity (& multiple entities) using a QID (unique Wikimedia identifier) and prints its title.</li>
+  * <li> Fetches data for multiple entities using site-key ("enwiki") and multiple titles and prints their QID's </li>
+  * <li> Searches for entities using a query (e.g. "Douglas Adams") and which language Wiki (or "IRI") to search, then prints the QID's and labels of the results. </li>
+  * <li> Fetches data for a single entity using its QID, but limits the volume of data returned by first applying site-link, language, and property filters</li>
+  *</ul>
+  * 
+  *
+  * @throws MediaWikiApiErrorException
+  * @throws IOException 
 
+  * @author Markus Kroetzsch
+ */
+
+public class FetchOnlineDataExample {
 	public static void main(String[] args) throws MediaWikiApiErrorException, IOException {
 		ExampleHelpers.configureLogging();
-		printDocumentation();
+		FetchOnlineDataExample.printDocumentation();
 
 		WikibaseDataFetcher wbdf = new WikibaseDataFetcher(
 				BasicApiConnection.getWikidataApiConnection(),
 				Datamodel.SITE_WIKIDATA);
 
+		FetchOnlineDataExample.fetchSingleEntityByQID(wbdf);
+		FetchOnlineDataExample.fetchEntitiesByTitles(wbdf);
+		FetchOnlineDataExample.fetchEntityResultsBySearchTerm(wbdf);
+		FetchOnlineDataExample.fetchEntityWithAppliedFilters(wbdf);
+	}
+
+	/**
+	 * Fetches data for a single entity using its QID (unique identifier).
+	 * The fetched data is then written to selected ouputs and prints the 'en' text title to the console.	
+	 */
+	public static void fetchSingleEntityByQID(WikibaseDataFetcher wbdf) throws MediaWikiApiErrorException, IOException {
 		System.out.println("*** Fetching data for one entity:");
-		EntityDocument q42 = wbdf.getEntityDocument("Q42");
-		System.out.println(q42);
 
-		if (q42 instanceof ItemDocument) {
-			System.out.println("The English name for entity Q42 is "
-					+ ((ItemDocument) q42).getLabels().get("en").getText());
+		// Multiple entities can be fetched in one go by providing a list of QID's & using the getEntityDocuments() method.
+		EntityDocument entityDocument = wbdf.getEntityDocument("Q42");
+
+		if (entityDocument instanceof ItemDocument) {
+			System.out.println("The English name for entity Q42 is: "
+					+ ((ItemDocument) entityDocument).getLabels().get("en").getText());
 		}
+	}
 
-		System.out.println("*** Fetching data for several entities:");
-		Map<String, EntityDocument> results = wbdf.getEntityDocuments("Q80",
-				"P31");
-		// Keys of this map are Qids, but we only use the values here:
-		for (EntityDocument ed : results.values()) {
-			System.out.println("Successfully retrieved data for "
-					+ ed.getEntityId().getId());
+	/**
+	 * Fetches data for multiple enetities using their titles.
+	 * The fetched data is then written to selected ouputs and prints the unique QID to the console.
+	 */
+	public static void fetchEntitiesByTitles(WikibaseDataFetcher wbdf) throws MediaWikiApiErrorException, IOException {
+		System.out.println("*** Fetching data for entities by page titles:");
+
+		// Similar to above, single entities can be fetched by providing a single title & using the getEntityDocumentsByTitle() method.
+		Map<String, EntityDocument> entityDocuments = wbdf.getEntityDocumentsByTitle("enwiki", "Terry Pratchett",
+				"Neil Gaiman");
+
+		for (Entry<String, EntityDocument> entry : entityDocuments.entrySet()) {
+			System.out.println("The QID for the entity with page title \""
+					+ entry.getKey() + "\" is: " + entry.getValue().getEntityId().getId());
 		}
+	}
 
-		System.out
-				.println("*** Fetching data using filters to reduce data volume:");
-		// Only site links from English Wikipedia:
-		wbdf.getFilter().setSiteLinkFilter(Collections.singleton("enwiki"));
-		// Only labels in French:
-		wbdf.getFilter().setLanguageFilter(Collections.singleton("fr"));
-		// No statements at all:
-		wbdf.getFilter().setPropertyFilter(Collections.emptySet());
+	/**
+	 * Fetches results data for whichever entities match the search term for language code provided.
+	 * The resultant data is then written to selected ouputs and prints the QID and label of the search results to the console.
+	 */
+	public static void fetchEntityResultsBySearchTerm(WikibaseDataFetcher wbdf)
+			throws MediaWikiApiErrorException, IOException {
+		System.out.println("*** Doing search on Wikidata for: Douglas Adams");
+
+		// Search for entities using a search term and language code.
+		List<WbSearchEntitiesResult> searchResults = wbdf.searchEntities("Douglas Adams", "fr");
+		for (WbSearchEntitiesResult result : searchResults) {
+			System.out.println("Found entity with QID " + result.getEntityId() + " and label \""
+					+ result.getLabel() + "\".");
+		}
+	}
+
+	/**
+	 * Fetches data for a single entity using its QID and applies filters to reduce the volume of data returned.
+	 * The fetched data is then written to selected ouputs and prints the French label and English Wikipedia page title to the console.
+	 */
+	public static void fetchEntityWithAppliedFilters(WikibaseDataFetcher wbdf)
+			throws MediaWikiApiErrorException, IOException {
+		System.out.println("*** Fetching data using filters to reduce data volume:");
+
+		// apply filters to data to get only what we want...
+		wbdf.getFilter().setSiteLinkFilter(Collections.singleton("enwiki")); // Only site links from English Wikipedia
+		wbdf.getFilter().setLanguageFilter(Collections.singleton("fr")); // Only labels in French
+		wbdf.getFilter().setPropertyFilter(Collections.emptySet()); // No statements at all
+		// Fetch the entity data
 		EntityDocument q8 = wbdf.getEntityDocument("Q8");
+
 		if (q8 instanceof ItemDocument) {
 			System.out.println("The French label for entity Q8 is "
 					+ ((ItemDocument) q8).getLabels().get("fr").getText()
 					+ "\nand its English Wikipedia page has the title "
-					+ ((ItemDocument) q8).getSiteLinks().get("enwiki")
-					.getPageTitle() + ".");
+					+ ((ItemDocument) q8).getSiteLinks().get("enwiki").getPageTitle() + ".");
 		}
-
-		System.out.println("*** Fetching data based on page title:");
-		EntityDocument edPratchett = wbdf.getEntityDocumentByTitle("enwiki",
-				"Terry Pratchett");
-		System.out.println("The Qid of Terry Pratchett is "
-				+ edPratchett.getEntityId().getId());
-
-		System.out.println("*** Fetching data based on several page titles:");
-		results = wbdf.getEntityDocumentsByTitle("enwiki", "Wikidata",
-				"Wikipedia");
-		// In this case, keys are titles rather than Qids
-		for (Entry<String, EntityDocument> entry : results.entrySet()) {
-			System.out
-					.println("Successfully retrieved data for page entitled \""
-							+ entry.getKey() + "\": "
-							+ entry.getValue().getEntityId().getId());
-		}
-
-		System.out.println("** Doing search on Wikidata:");
-		for(WbSearchEntitiesResult result : wbdf.searchEntities("Douglas Adams", "fr")) {
-			System.out.println("Found " + result.getEntityId() + " with label " + result.getLabel());
-		}
-
-		System.out.println("*** Done.");
 	}
 
 	/**
-	 * Prints some basic documentation about this program.
-	 */
+	* Prints some basic documentation about this program.
+	*/
 	public static void printDocumentation() {
 		System.out
 				.println("********************************************************************");
 		System.out.println("*** Wikidata Toolkit: FetchOnlineDataExample");
 		System.out.println("*** ");
 		System.out
-				.println("*** This program fetches individual data using the wikidata.org API.");
+				.println(
+						"*** This example demonstrates fetching data from the Wikidata.org API by their QID, title, or query and applying various filters to reduce the volume of data returned.");
 		System.out.println("*** It does not download any dump files.");
 		System.out
 				.println("********************************************************************");
 	}
-
 }
